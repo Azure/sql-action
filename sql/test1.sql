db28ce1677c9d7ed3bf2f767f722585e6f3b56ff
Raymond Mulligan <64546678+ramullig@users.noreply.github.com>
2020-05-27 11:02:28 +0100

    Create test1.sql

---
+++ sql/test1.sql
@@ -0,0 +1,12 @@
+ALTER TABLE [SalesLT].[Customer]
+DROP COLUMN Test1;
+
+ALTER TABLE  [SalesLT].[Customer]
+ADD Test1 varchar(255);
+
+Update [SalesLT].[Customer]
+Set Test1 = 'ray'
+WHERE CustomerID = 1;
+
+select Test1 from [SalesLT].[Customer]
+WHERE CustomerID = 1;
