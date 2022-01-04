782eeccc5e18f83b38eed048a6a79638acb7a849
Zi Chen <zijchen@microsoft.com>
2022-01-04 13:54:30 -0800

    Truncate table instead of dropping DB

--- __testdata__/testsql.sql
+++ __testdata__/testsql.sql
@@ -3,5 +3,5 @@
 -- This should successfully insert data into the table created in the DACPAC step
 INSERT INTO [Table1] VALUES(1, 'test');
 
--- Cleanup test database afterwards
-DROP DATABASE [sql-action];
+-- Cleanup afterwards
+TRUNCATE TABLE [Table1];
