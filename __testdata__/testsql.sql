998da415f0d516f4e5de8c8b436e345825d72281
Zi Chen <zijchen@microsoft.com>
2022-01-04 13:32:49 -0800

    Escape database name

--- __testdata__/testsql.sql
+++ __testdata__/testsql.sql
@@ -1,7 +1,7 @@
 -- This script is used by pr-check.yml to test the SQLCMD action
 
 -- This should successfully insert data into the table created in the DACPAC step
-INSERT INTO Table1 VALUES(1, 'test');
+INSERT INTO [Table1] VALUES(1, 'test');
 
 -- Cleanup test database afterwards
-DROP DATABASE sql-action;
+DROP DATABASE [sql-action];
