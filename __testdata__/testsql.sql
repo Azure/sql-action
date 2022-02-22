4b444432279dc3ec2648dcc897d209c77dc9d6a9
Z Chen <13544267+zijchen@users.noreply.github.com>
2022-02-22 10:22:26 -0800

    Add DACPAC action test to PR check (#63)
    * Add DACPAC action test to PR check
    
    * Escape database name
    
    * Truncate table instead of dropping DB
    
    * Cleanup
    
    * Set dbName at runtime, drop afterwards
    
    * Fix pr-check.yml
    
    * Fix pr-check.yml
    
    * Use Initial Catalog instead of Database
    
    * Use master DB for SQLCMD action
    
    * Separate DB cleanup to its own script
    
    * Use single quotes on pr-check run condition

---
+++ __testdata__/testsql.sql
@@ -0,0 +1,4 @@
+-- This script is used by pr-check.yml to test the SQLCMD action
+
+-- This should successfully insert data into the table created in the DACPAC step
+INSERT INTO [Table1] VALUES(1, 'test');
