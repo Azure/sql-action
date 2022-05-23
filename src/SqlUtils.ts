ad0198acf908bdb7e1fad04bb0c2480a25279cfd
Zi Chen <zijchen@microsoft.com>
2022-05-23 13:13:07 -0700

    Fix mssql connection

--- src/SqlUtils.ts
+++ src/SqlUtils.ts
@@ -5,22 +5,26 @@ import SqlConnectionStringBuilder from "./SqlConnectionStringBuilder";
 
 export default class SqlUtils {
     static async detectIPAddress(connectionString: SqlConnectionStringBuilder): Promise<string> {
+        core.debug(`Validating if client '${process.env.computername}' has access to Sql Server '${connectionString.server}'.`);
         let ipAddress = '';
-
-        try {
-            core.debug(`Validating if client '${process.env.computername}' has access to Sql Server '${connectionString.server}'.`);
-            await mssql.connect(connectionString.connectionString);
-        }
-        catch (error) {
-            core.debug(error.message);
-            const ipAddresses = error.message.match(Constants.ipv4MatchPattern);
-            if (!!ipAddresses) {
-                ipAddress = ipAddresses[0];
+        const pool = new mssql.ConnectionPool({
+            server: connectionString.server,
+            database: 'master',
+            user: connectionString.userId,
+            password: connectionString.password
+        });
+        await pool.connect(error => {
+            if (!!error) {
+                core.debug(error.message);
+                const ipAddresses = error.message.match(Constants.ipv4MatchPattern);
+                if (!!ipAddresses) {
+                    ipAddress = ipAddresses[0];
+                }
+                else {
+                    throw new Error(`Failed to add firewall rule. Unable to detect client IP Address. ${error}`)
+                }
             }
-            else {
-                throw new Error(`Failed to add firewall rule. Unable to detect client IP Address. ${error}`)
-            }
-        }
+        });
 
         //ipAddress will be an empty string if client has access to SQL server
         return ipAddress;
