203615f9651bce2c7c72bba032f5e5ada2f2269a
Zi Chen <zijchen@microsoft.com>
2022-06-01 12:59:11 -0700

    Restore connection string validation regex

--- src/SqlConnectionConfig.ts
+++ src/SqlConnectionConfig.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core';
 import { config, ConnectionPool } from "mssql";
+import Constants from './Constants';
 
 /**
  * Wrapper class for the mssql.config object.
@@ -9,13 +10,10 @@ export default class SqlConnectionConfig {
     private _connectionString: string;
 
     constructor(connectionString: string) {
-        this._connectionString = connectionString;
+        this._validateConnectionString(connectionString);
 
-        try {
-            this._connectionConfig = ConnectionPool.parseConnectionString(connectionString);
-        } catch (error) {
-            throw new Error('Invalid connection string. A valid connection string is a series of keyword/value pairs separated by semi-colons. If there are any special characters like quotes, semi-colons in the keyword value, enclose the value within quotes. Refer this link for more info on conneciton string https://aka.ms/sqlconnectionstring');
-        }
+        this._connectionString = connectionString;
+        this._connectionConfig = ConnectionPool.parseConnectionString(connectionString);
 
         // masking the connection string password to prevent logging to console
         if (this._connectionConfig.password) {
@@ -31,6 +29,33 @@ export default class SqlConnectionConfig {
 
     public get ConnectionString(): string {
         return this._connectionString;
+    }
+
+    /**
+     * The basic format of a connection string includes a series of keyword/value pairs separated by semicolons. 
+     * The equal sign (=) connects each keyword and its value. (Ex: Key1=Val1;Key2=Val2)
+     * 
+     * Following rules are to be followed while passing special characters in values:
+            1. To include values that contain a semicolon, single-quote character, or double-quote character, the value must be enclosed in double quotation marks. 
+            2. If the value contains both a semicolon and a double-quote character, the value can be enclosed in single quotation marks. 
+            3. The single quotation mark is also useful if the value starts with a double-quote character. Conversely, the double quotation mark can be used if the value starts with a single quotation mark. 
+            4. If the value contains both single-quote and double-quote characters, the quotation mark character used to enclose the value must be doubled every time it occurs within the value.
+        
+        Regex used by the parser(connectionStringParserRegex) to parse the VALUE:
+            
+            ('[^']*(''[^']*)*') -> value enclosed with single quotes and has consecutive single quotes 
+            |("[^"]*(""[^"]*)*") -> value enclosed with double quotes and has consecutive double quotes
+            |((?!['"])[^;]*)) -> value does not start with quotes does not contain any special character. Here we do a positive lookahead to ensure that the value doesn't start with quotes which should have been handled in previous cases
+        Regex used to validate the entire connection string:
+        
+        A connection string is considered valid if it is a series of key/value pairs separated by semicolons. Each key/value pair must satisy the connectionStringParserRegex to ensure it is a valid key/value pair.
+        ^[;\s]*{KeyValueRegex}(;[;\s]*{KeyValueRegex})*[;\s]*$
+        where KeyValueRegex = ([\w\s]+=(?:('[^']*(''[^']*)*')|("[^"]*(""[^"]*)*")|((?!['"])[^;]*))))
+    */
+    private _validateConnectionString(connectionString: string) {
+        if (!Constants.connectionStringTester.test(connectionString)) {
+            throw new Error('Invalid connection string. A valid connection string is a series of keyword/value pairs separated by semi-colons. If there are any special characters like quotes, semi-colons in the keyword value, enclose the value within quotes. Refer this link for more info on conneciton string https://aka.ms/sqlconnectionstring');
+        }
     }
 
     private _validateconfig(): void {
