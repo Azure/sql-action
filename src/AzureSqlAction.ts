439b52fd0f7c00fe5c971390e5857c79809ef9b1
Z Chen <13544267+zijchen@users.noreply.github.com>
2024-06-27 10:24:17 -0700

    Escape double quotes in connection string (#230)
    * Escape double quotes in connection string
    
    * Build main.js
    
    * Double up double quotes
    
    * Fix tests
    
    * Add some tests
    
    * Add comment

--- src/AzureSqlAction.ts
+++ src/AzureSqlAction.ts
@@ -128,10 +128,10 @@ export default class AzureSqlAction {
             case SqlPackageAction.Publish: 
             case SqlPackageAction.Script:
             case SqlPackageAction.DeployReport:
-                args += `/Action:${SqlPackageAction[inputs.sqlpackageAction]} /TargetConnectionString:"${inputs.connectionConfig.ConnectionString}" /SourceFile:"${inputs.filePath}"`;
+                args += `/Action:${SqlPackageAction[inputs.sqlpackageAction]} /TargetConnectionString:"${inputs.connectionConfig.EscapedConnectionString}" /SourceFile:"${inputs.filePath}"`;
                 break;
             case SqlPackageAction.DriftReport:
-                args += `/Action:${SqlPackageAction[inputs.sqlpackageAction]} /TargetConnectionString:"${inputs.connectionConfig.ConnectionString}"`;
+                args += `/Action:${SqlPackageAction[inputs.sqlpackageAction]} /TargetConnectionString:"${inputs.connectionConfig.EscapedConnectionString}"`;
                 break;
 
             default:
